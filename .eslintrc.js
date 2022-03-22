7a2bc5953a31a8dc01f21f2932146af9803c8d2b
Nico Mac <nicomac.me@gmail.com>
2022-03-22 21:28:39 +0800

    Build Test 3!

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,16 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
+  extends: ['plugin:vue/essential', '@vue/standard'],
   parserOptions: {
-    parser: 'babel-eslint',
+    ecmaVersion: 2020
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
-};
+    camelcase: 'off',
+    'space-before-function-paren': 'off'
+  }
+}
